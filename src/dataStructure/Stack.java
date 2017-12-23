bf81738678c2417e7054fc91a3b20fb7596a5123
donley828 <344919627@qq.com>
2017-12-23 12:47:18 +0800

    stack(linked list)

---
+++ src/dataStructure/Stack.java
@@ -0,0 +1,43 @@
+package dataStructure;
+
+public class Stack<Item> {
+	private Node first; // 栈顶
+	private int N; // 元素数量
+
+	private class Node {
+		// 定义了节点的内部类
+		Item item;
+		Node next;
+	}
+
+	public boolean isEmpty() {
+		return first == null; // 或: N == 0
+	}
+
+	public int size() {
+		return N;
+	}
+
+	public void push(Item item) {
+		// 添加元素
+		Node oldfirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldfirst;
+		N++;
+	}
+
+	public Item pop() {
+		// 从栈顶删除元素
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
+	}
+
+	public static void main(String[] args) {
+		Stack<String> s = new Stack<String>();
+		s.push("test");
+		System.out.println(s.size());
+	}
+}
